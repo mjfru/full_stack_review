bd761d33608067cface2ca328d4ed35cde7fadc0
Matt Frutchey <mjfru201@gmail.com>
2024-03-15 10:16:50 -0400

    begin js review, again, and make initial note page for basics

---
+++ js_review/js_notes/01basics_review.js
@@ -0,0 +1,20 @@
+/* 
+! JS Basics
+! Only new or information I would like to reinforce will be included in these notes
+
+! Primitive Types in JS
+
+* Numbers
+  - No floats, all numbers (positive and negative) are included.
+  - NaN is also included in 'numbers' despite being...not a number!
+*/
+
+console.log(typeof NaN); // number
+
+/*
+
+! Variables
+* Increment Operators
+?   - i++ - This returns the current value of the variable and THEN increments by one.
+?   - ++i - The pre-increment operator, increments the variables value by one FIRST and then returns it.
+*/
