0dfdbc8370bac35e9131970709cd118804b50dc4
Matt Frutchey <mjfru201@gmail.com>
2024-03-16 10:30:10 -0400

    add constants and strings course exercises

---
+++ js_review/js_exercises/03strings.js
@@ -0,0 +1,12 @@
+/*
+! String Variables Practice
+Please define the following string variables (you can use either let or const):
+
+bestColor should be set to the string purple 
+
+quote should be set to the string You had me at "hello" - be careful about quotation marks, we want double quotes inside the string.
+*/
+
+let bestColor = "purple";
+
+const quote = 'You had me at "hello"'
