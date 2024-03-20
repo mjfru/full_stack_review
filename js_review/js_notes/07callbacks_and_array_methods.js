21ffa9c13454a6920f2fa2e730383b64d7afb008
Matt Frutchey <mjfru201@gmail.com>
2024-03-20 09:53:18 -0400

    restructure callback notes

--- js_review/js_notes/07callbacks_and_array_methods.js
+++ js_review/js_notes/07callbacks_and_array_methods.js
@@ -1,6 +1,88 @@
 /*
 ! Callbacks & Array Methods
+/*
 
+! Higher Order Functions - Functions that operate on / with other functions.
+* They can: accept other functions as arguments and also return a function.
+*/
+
+//? Higher Order Function Example:
+let rollADie = function () {
+  const roll = Math.floor(Math.random() * 6) + 1;
+  console.log(roll);
+};
+
+function callTwice(func) {
+  func();
+  func();
+}
+
+callTwice(rollADie);
+
+/*
+! Returning Functions from Functions
+*/
+
+//? Examples of functions returning / generating functions
+function makeMysteryFunc() {
+  const rand = Math.random();
+  if (rand > 0.5) {
+    return function () {
+      console.log("Congrats, I am a good function!");
+    };
+  } else {
+    return function () {
+      console.log("Not today, buddy.");
+    };
+  }
+}
+
+const mysteryFunc = makeMysteryFunc();
+mysteryFunc();
+
+//? Making a 'factory' function
+function makeBetweenFunc(min, max) {
+  return function (num) {
+    return num >= min && num <= max;
+  };
+}
+
+// How we can utilize having made a factory function and passing a value to the function it returns:
+const isChild = makeBetweenFunc(0, 18);
+console.log(isChild(15)); // true
+console.log(isChild(35)); // false
+
+/*
+! Methods - Functions added as properties to objects
+? Since this is SO common, there is a new shorthand available for us to us as well.
+*/
+
+//? Original Way:
+const myMath = {
+  PI: 3.14159,
+  square: function (num) {
+    return num * num;
+  },
+  cube: function (num) {
+    return num * num * num;
+  },
+};
+
+console.log(myMath.cube(3)); // 27
+
+//? Newer Shortcut:
+const newMath = {
+  greeting: 'Hi',
+  add(x, y) {   //! Note: no function keyword!
+    return x + y;
+  },
+  multiply(x, y) {
+    return x * y;
+  }
+}
+console.log(newMath.multiply(8, 5)); // 40
+
+/*
 ! forEach method
 * This method accepts a callback function and will call the function once - per element - in the array.
 ? This was more prevalent before the for...of loop came into existence.
