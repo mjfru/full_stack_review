6848a961197d4cec17f0da0bc8bf253c7ecd096f
Matt Frutchey <mjfru201@gmail.com>
2024-04-01 13:27:04 -0400

    add general notes about DBs and Mongo

---
+++ backend_development/backend_notes/05mongo_db_review.js
@@ -0,0 +1,23 @@
+/*
+! (Re)Introduction to Mongo DB
+What is Mongo? - The Database we'll be using from here on out and one of the components of the -M-ERN stack.
+* Mongo is a document database, commonly used in combination with Node.js, and allows us to store and retrieve complex data.
+
+? Why use a database in the first place instead of just saving to a file?
+  - Databases can handle large amounts of data efficiently and store it compactly.
+  - They provide tools for easy insertion, querying, and updating of data.
+  - They generally offer security features and control over access to data.
+  - They (generally...and hopefully) scale well!
+
+! Databases usually fall under two categories: SQL and NoSQL
+* SQL (Structured Query Language) databases are relational databases; we pre-define a schema of tables before we insert anything.
+  ? Examples: MySQL, Postgres, SQLite, Oracle, MS SQL
+* NoSQL is well, the opposite and include document, key-value, and graph stores databases.
+  ? Examples: MongoDB, Couch DB, Neo4j, Cassandra, Redis
+
+! So, why Mongo DB?
+  * Very commonly used with Node and Express (MEAN and MERN stacks)
+  Easy to get started with
+  Plays particularly well with JavaScript
+  Its popularity also means there is a strong community of developers using Mongo.
+*/
