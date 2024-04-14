9419bbad95deaeac665b4af6c1f2b361936a3b9e
Matt Frutchey <mjfru201@gmail.com>
2024-04-14 08:59:13 -0400

    include authorization and redirection for new, edit, and delete routes

--- intents_project/seeds/index.js
+++ intents_project/seeds/index.js
@@ -19,6 +19,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 30) + 10;
     const camp = new Campground({
+      author: '66194a042459ced629eecfb5',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: 'https://source.unsplash.com/collection/483251',
